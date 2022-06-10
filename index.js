554876df431886ced7ebb0d2d072ba3042add8f8
Irvin Martinez <irvin0329@outlook.com>
2022-06-10 11:16:53 -0600

    se creo la carpeta models

--- index.js
+++ index.js
@@ -3,35 +3,28 @@ require("dotenv").config(); // las variables del .env se agreagan a process.env
 const express = require("express");
 const mongoose = require("mongoose");
 
+const Post = require('./models/post.model')
+
 //*Inicializamos constantes con la configuracion
-
-// const PORT = process.env.PORT;
-
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
+
 const PORT = process.env.PORT;
 
-// const DB_NAME = process.env.DB_HOST;
 const URL =  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 
-//Schema define las reglas del modelo
-const postSchema = new mongoose.Schema({
-    avatar: String,
-    banner: String,
-    dia: Number,
-    mes: Number,
-    año: Number,
-    title: String,
-    tags: String
+
+app.get("/posts", async (req,res)=>{
+    //Leer posts de base de datos
+    const posts = await Post.find({})
+    // console.log(posts)
+    res.json(posts)
 })
 
-//modelo creado
-const Post = mongoose.model('Posts', postSchema)
-//modelo instanciado
 
 mongoose
     .connect(URL)
@@ -50,14 +43,6 @@ mongoose
 
         //  await Post.create(newPost)
         //  console.log("koder created")
-
-
-        //Leer posts de base de datos
-         const posts = await Post.find({})
-         console.log(posts)
-
-
-
         app.listen(PORT, () => {
             console.log("Server ejecutandose en el puerto:", PORT);
           });
